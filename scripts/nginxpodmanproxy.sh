fae7795a73593ea5764c41fa1c139108c9c284d4
JiwaArch <2511512002_muhammad@unand.ac.id>
2026-06-21 13:45:32 +0700

    fix typo scrpit

--- scripts/nginxpodmanproxy.sh
+++ scripts/nginxpodmanproxy.sh
@@ -2,7 +2,7 @@
 
 rm /etc/nginx/sites-enabled/podmanproxy.conf
 rm /etc/nginx/sites-avaliable/podmanproxy.conf
-cp ./configs/nginx/podmanproxy.conf /etc/nginx/sites-avaliable/podmanproxy.conf
+cp ./configs/nginx/podmanproxy.conf /etc/nginx/sites-available/podmanproxy.conf
 sudo ln -s /etc/nginx/sites-available/podmanproxy.conf /etc/nginx/sites-enabled/
 sudo nginx -t
 sudo systemctl reload nginx
