2fec78e05a047d020f6094980db89cfd4a470d9f
JiwaArch <2511512002_muhammad@unand.ac.id>
2026-06-21 13:54:28 +0700

    fix port on podman

--- scripts/capserverup.sh
+++ scripts/capserverup.sh
@@ -4,4 +4,4 @@ podman stop capstonegoserver
 podman container rm capstonegoserver
 podman volume prune
 podman build -t capstonegoserver ./images/capstonegoserver/
-podman run -d -p 127.0.0.1:8080:3000 --name capstonegoserver capstonegoserver
+podman run -d -p 127.0.0.1:8080:8080 --name capstonegoserver capstonegoserver
