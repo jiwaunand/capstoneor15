e4698d820e2c381c96dd7a4dbc0390d0732e49dd
JiwaArch <2511512002_muhammad@unand.ac.id>
2026-06-21 13:40:01 +0700

    fix nginx script

--- scripts/nginxpodmanproxy.sh
+++ scripts/nginxpodmanproxy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 cp ./configs/nginx/podmanproxy.conf /etc/nginx/sites-avaliable/podmanproxy.conf
-sudo ln -s /etc/nginx/sites-available/go-server /etc/nginx/sites-enabled/
+sudo ln -s /etc/nginx/sites-available/podmanproxy.conf /etc/nginx/sites-enabled/
 sudo nginx -t
 sudo systemctl reload nginx
